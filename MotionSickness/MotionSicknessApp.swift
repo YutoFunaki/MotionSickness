f360b49834a74ee3165d943ea605602dba3070da
YutoFunaki <kaizuka0629@gmail.com>
2023-02-20 19:48:21 +0900

    Initial Commit

---
+++ MotionSickness/MotionSicknessApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionSicknessApp.swift
+//  MotionSickness
+//
+//  Created by 船木勇斗 on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct MotionSicknessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
